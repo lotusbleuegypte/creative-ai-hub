fc85e7a184e2df6cc0173d78167cd57cbd0a6392
lotusbleuegypte <lotusbleuegypte@gmail.com>
2025-07-03 22:49:41 +0200

    Update generate-music.js

--- pages/api/generate-music.js
+++ pages/api/generate-music.js
@@ -27,7 +27,8 @@ export default async function handler(req, res) {
     try {
       console.log('🎵 Génération audio avec Hugging Face...');
       
-      const response = await fetch('https://api-inference.huggingface.co/models/facebook/musicgen-small', {
+      const response = await fetch('https://api-inference.huggingface.co/models/facebook/musicgen-stereo-large
+', {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${token}`,
